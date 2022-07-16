1f6df65eb0e2403c7b8570e7e354ac6c64e568c4
Saï <saipatateyt@gmail.com>
2022-07-16 15:57:09 +0200

    update SEO

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,26 @@ const Layout = ({ children }) => {
     return (
     <>
         <Head>
+            <meta charset="utf-8" />
             <title>Saï - Développeur Frontend</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="author" content="Saï" />
+            <meta property="site_name" content="Saï" />
+            <meta property="title" content="Saï - Développeur Frontend" />
+            <meta property="image" content="/portfolio/sai.png" />
+            <meta name="description" content="Mon portfolio, regroupe tous mes projets" />
+            <meta property="og:type" content="website" />
+            <meta property="og:site_name" content="Saï" />
+            <meta property="og:url" content="https://ewha.fr/" />
+            <meta property="og:title" content="Mon Portfolio" />
+            <meta property="og:description" content="Mon Portfolio - Regroupe tous mes projets" />
+            <meta property="og:image" content="/portfolio/sai.png" />
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://ewha.fr" />
+            <meta property="twitter:title" content="Mon Portfolio" />
+            <meta property="twitter:description" content="Mon Portfolio - Regroupe tous mes projets" />
+            <meta property="twitter:image" content="/portfolio/sai.png" />
+            <meta name="keywords" content="ewen, saï, saipatate, web, developer, développeur, frontend, fantashop" />
         </Head>
 
         <Navbar />
